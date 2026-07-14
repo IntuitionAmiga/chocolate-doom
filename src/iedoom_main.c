5dc75d28cd1ae814bc8526ee50a945465908e2db
Zayn Otley <intuitionamiga@gmail.com>
2026-07-14 18:54:37 +0200

    iedoom: boot to title/attract loop by default
    Drop the -warp/-skill autostart args from the freestanding entry so the
    default iedoom.ie68/iedoom.ie86 builds boot to the title screen and demo
    attract loop, matching original DOOM behaviour. Timedemo variants keep
    their -timedemo demo1 argv.

--- src/iedoom_main.c
+++ src/iedoom_main.c
@@ -11,27 +11,17 @@ extern char **myargv;
 static char iedoom_arg0[] = "iedoom";
 static char iedoom_arg_iwad[] = "-iwad";
 static char iedoom_arg_iwad_path[] = "doom1.wad";
-static char iedoom_arg_skill[] = "-skill";
-static char iedoom_arg_skill_level[] = "1";
-static char iedoom_arg_warp[] = "-warp";
-static char iedoom_arg_episode[] = "1";
-static char iedoom_arg_map[] = "1";
 static char *iedoom_argv[] =
 {
     iedoom_arg0,
     iedoom_arg_iwad,
     iedoom_arg_iwad_path,
-    iedoom_arg_skill,
-    iedoom_arg_skill_level,
-    iedoom_arg_warp,
-    iedoom_arg_episode,
-    iedoom_arg_map,
     NULL,
 };
 
 int iedoom_main(void)
 {
-    myargc = 8;
+    myargc = 3;
     myargv = iedoom_argv;
 
     M_FindResponseFile();
